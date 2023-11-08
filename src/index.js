995306bef4ae472bf3d635d3e8b23724b71e2109
Paul Fontes <paulfontes1@gmail.com>
2023-11-07 22:49:28 -0300

    Configuração de Cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const knex = require('./conexao')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    return res.json('Api está ok!')
+    return res.json('Api está ok - HEROKU CLI!')
 })
 
 const port = process.env.PORT || 3000
